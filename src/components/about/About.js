f2b8e32f2155565e7f55c4087c2ad6cc63feefd2
Cody Fackrell <codyfackrell56@gmail.com>
2024-08-14 21:24:22 -0600

    about section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -11,25 +11,30 @@ export default function About() {
       flexDirection={"column"}
       alignItems={"center"}
       mx={{ xs: "3rem", md: "10rem" }}
-      minHeight={"100vh"} 
+      minHeight={"100vh"}
     >
-            <Box width={{xs: '75%', md: "80%"}} height={{xs: '400px', md: '100%'}} bgcolor="#d4eefc" borderRadius="32px" pt={"50px"}>
-
-      <h3>About Me</h3>
-      <p>{info.bio}</p>
-      </Box>
+      {/* <Box
+        width={{ xs: "75%", md: "80%" }}
+        height={{ xs: "400px", md: "100%" }}
+        bgcolor="#d4eefc"
+        borderRadius="32px"
+        pt={"50px"}
+      > */}
+        <h3>About Me</h3>
+        <p>{info.bio}</p>
+      {/* </Box> */}
       <br></br>
       <h3>Skills</h3>
       <Box>
         <Grid
           container
           rowSpacing={4}
-          display={'flex'}
-          justifyContent={'center'}
+          display={"flex"}
+          justifyContent={"center"}
         >
           {info.skills.map((skill) => (
-            <Grid item container justifyContent={"center"} xs={6} md={2} >
-              <img src={skill.link} alt={skill.name} key={skill.name}/>
+            <Grid item container justifyContent={"center"} xs={6} md={2}>
+              <img src={skill.link} alt={skill.name} key={skill.name} />
             </Grid>
           ))}
         </Grid>
