498eb7023ce4fde16220b217ac09dfce392e447c
Cody Fackrell <codyfackrell56@gmail.com>
2025-09-24 19:19:18 -0600

    resume button

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -28,9 +28,9 @@ export default function Home() {
                {info.socials.map((social, index) => (
                   <SocialIcon key={index} link={social.link} icon={social.icon} label={social.label} />
                ))}
-               <Box p={1} border={'1.5px solid black'} borderRadius={'10px'} fontSize={'1rem'}>
+               <a href={"../../public/cody-fackrell-resume.pdf"} p={1} border={'1.5px solid black'} borderRadius={'10px'} fontSize={'1rem'}>
                   resume
-                </Box>
+               </a>
             </Box>
          </Box>
       </Box>
